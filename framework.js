58a46beb70eb37dade8e0546e4c808a6de02d5a9
Kiran A K <krnak07@gmail.com>
2020-09-08 10:54:40 +0530

    test -vscode

--- framework.js
+++ framework.js
@@ -14,6 +14,8 @@ app.use(function(req, res, next) {
     next();
 });
 
+console.log('kiran');
+
 // Set static directory before defining routes
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/node_modules', express.static(__dirname + '/node_modules'));
